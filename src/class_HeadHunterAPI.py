cc7c7a5eb6605b7422c509f5f3984d6d807ff918
vitek <rgavih@yandex.ru>
2024-05-14 00:22:18 +0700

    commit for 14.05.24

--- src/class_HeadHunterAPI.py
+++ src/class_HeadHunterAPI.py
@@ -2,10 +2,9 @@ from abc import ABC, abstractmethod
 import requests
 from progress_bar import progress_bar
 
-
 class AbstractVacancyAPI(ABC):
     @abstractmethod
-    def get_vacancies(self, keyword):
+    def get_vacancies(self, keyword, count):
         pass
 
 
@@ -15,25 +14,24 @@ class HhRuVacancyAPI(AbstractVacancyAPI):
         self.params = {
             "text": '',
             "area": 113,  # Код региона (113 - РФ)
-            "page": 0,  #номер страници
+            "page": 0,  # номер страницы
             "only_with_salary": True,  # Только вакансии с указанной зарплатой
             "per_page": 100  # Количество вакансий на странице
         }
-        self.vacancies = []
 
-
-    def get_vacancies(self, keyword):
+    def get_vacancies(self, keyword, count):
         self.params['text'] = keyword
-        while self.params.get('page') != 1:
+        total_vacancies = []
+        while len(total_vacancies) < count:
             response = requests.get(self.url, params=self.params)
             data = response.json()['items']
-            self.vacancies.extend(data)
+            total_vacancies.extend(data)
             self.params['page'] += 1
-        return self.vacancies
+        return total_vacancies[:count]
 
 
 if __name__ == '__main__':
     hh_api = HhRuVacancyAPI()
-    hh_vacancies = hh_api.get_vacancies("python developer")
+    hh_vacancies = hh_api.get_vacancies("python developer", 5)  # Задаем количество выводимых вакансий
     print(hh_vacancies)
     progress_bar(100)
--- src/vacancy.py
+++ src/vacancy.py
@@ -1,46 +1,77 @@
-from _ast import keyword
-
 from src.class_HeadHunterAPI import HhRuVacancyAPI
+from progress_bar import progress_bar
+from config import ROOT_DIR
+import json
+import os
 
 
-class Vacncy(HhRuVacancyAPI):
-    def __init__(self):
-        self.vacancies = HhRuVacancyAPI().get_vacancies(keyword)
+class Vacancy(HhRuVacancyAPI):
+    def __init__(self, id, name, url, salary, request):
+        self.__id = id
+        self.name = name
+        self.url = url
+        self.salary = salary
+        self.request = request
 
-    def cast_to_object_list(self.vacancies):
+    def __repr__(self):
+        return f'{self.name}, {self.url}, {self.salary}, {self.request}'
+
+    def __str__(self):
+        return f'Вакансия:{self.name}, зарплата: {self.salary}, требования: {self.request}'
+
+    def cast_to_object_list(self, hh_vacancies=HhRuVacancyAPI().get_vacancies("python", 5)):
         list_vacancies = []
-        for vacancy in vacancies:
-            id = vacancy["id"]  # количество вакансий
-            name = vacancy["name"]  # наименование вакансии
-            salary_from = vacancy["salary"]['from']  # зарплата от...
-            if salary_from != None:
+        amount_vacancy = len(hh_vacancies)
+        print(f'Найдено вакансий: {amount_vacancy}\n')
+        for vacancy in hh_vacancies:
+            id = vacancy["id"]
+            name = vacancy["name"]
+            salary_from = vacancy["salary"]['from']
+            if salary_from is not None:
                 salary_from = salary_from
             else:
                 salary_from = 0
-            salary_to = vacancy["salary"]['to']  # зарплата до...
-            if salary_to != None:
+            salary_to = vacancy["salary"]['to']
+            if salary_to is not None:
                 salary_to = salary_to
             else:
                 salary_to = 0
-            currency = vacancy["salary"]['currency']  #Валюта
+            currency = vacancy["salary"]['currency']
             if currency == 'RUR':
                 currency = 'RUB'
             else:
                 currency = currency
-            alternate_url = vacancy["alternate_url"]  # ссылка на вакансию
+            alternate_url = vacancy["alternate_url"]
             request = str(vacancy['snippet']['requirement']).replace("<highlighttext>", "").replace(
-                "</highlighttext>", "")  # Требования к вакансии
-            responsibilities = str(vacancy['snippet']['responsibility']).replace("<highlighttext>", "").replace(
-                "</highlighttext>", "")  #Обязанност сотрудника
-            area = vacancy["area"]["name"]  # Место работы
-            list_vacancies.append(vacancy)
-            return list_vacancies
-#             return f'''id вакансии: {id}\nНазвание: {name}\nМесто работы:{area}
-# Зарплата: от {salary_from} до {salary_to}, валюта:{currency}\nСсылка:{alternate_url}
-# Требования:{request}\nОбязанности:{responsibilities}\n'''
+                "</highlighttext>", "")
+            Responsibilities = str(vacancy['snippet']['responsibility']).replace("<highlighttext>", "").replace(
+                "</highlighttext>", "")
+            area = vacancy["area"]["name"]
+            list_vacancies.append({
+                "id": id,
+                "name": name,
+                "area": area,
+                "salary_from": salary_from,
+                "salary_to": salary_to,
+                "currency": currency,
+                "url": alternate_url,
+                "requirements": request,
+                "responsibilities": Responsibilities
+            })
+
+        # Запись данных в файл JSON
+        folder_path = ROOT_DIR
+        file_path = os.path.join(folder_path, 'data', "vacancies.json")
+        # Откройте файл для записи в указанной папке
+        with open(file_path, "w", encoding='utf-8') as f:
+            json.dump(list_vacancies, f, ensure_ascii=False, indent=4)
+
+        return list_vacancies
 
 
 if __name__ == '__main__':
-    vacncy = Vacncy()
-    print(vacncy)
-    print(vacncy.cast_to_object_list(vacancies))
+    vacancy = Vacancy('python', '', '80000-100000', 'опыт работы от 3х лет')
+    print(vacancy)
+    vac = HhRuVacancyAPI()
+    print(vacancy.cast_to_object_list(vac.get_vacancies("develop", 3)))
+    progress_bar(100)
